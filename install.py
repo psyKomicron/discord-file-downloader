1cc37a08f767c2a51cd92e79a1728c614f196d8f
psyKomicron <psykomicron@protonmail.com>
2023-03-05 14:27:44 +0100

    Trying to download realease

--- install.py
+++ install.py
@@ -10,6 +10,7 @@ SIMPLER_DOWNLOADER = True
 DEBUG = True
 PYTHON_PREFIX = "python3.11"
 EXIT_ON_FAILED_DEPENDENCY_INSTALL = False
+USE_GIT = False
 
 def mkdir(path: str) -> bool:
     if os.name == "posix":
@@ -20,6 +21,21 @@ def mkdir(path: str) -> bool:
             return os.system(f"mkd  ir {path}") == 0
     else:
         return False
+    
+def checkInstall(command: str) -> None:
+    shpath = shutil.which(command)
+    if not shpath or len(shpath) == 0:
+        print(f"{command} is not installed, install it and rerun the installation script.")
+        if os.name != "posix":
+            exit()
+        if input(f"Do you want to install {command} ? [y/n] ") == "n":
+            exit()
+
+        if os.system(f"sudo apt install {command}") != 0:
+            print(f"Failed to install {command}.")
+            exit()    
+        print(f"Successfully installed {command}.")
+    
 
 print("DiFD installer by psyKomicron")
 downloadPath = ""
@@ -35,62 +51,52 @@ if not os.path.exists(downloadPath):
     else:
         exit(-1)
 
-requiredUtils = ["git", "wget"]
-for reqUtil in requiredUtils:
-    shpath = shutil.which(reqUtil)
-    print(shpath)
-    if not shpath or len(shpath) == 0:
-        print(f"{reqUtil} is not installed, install it and rerun the installation script.")
-        if os.name != "posix":
-            exit()
-        if input(f"Do you want to install {reqUtil} ? [y/n] ") == "n":
-            exit()
-        if os.system(f"sudo apt install {reqUtil}") != 0:
-            print(f"Failed to install {reqUtil}.")
-            exit()    
-        print(f"Successfully installed {reqUtil}.")
-
-print("Cloning repository...")
-if not DEBUG and os.system(f"git clone {REPO_PATH}.git {downloadPath}") != 0:
-    print("Failed to clone repository, please check you internet connection and retry installation.")
-    exit(-1)
-print(f"Cloned repository into {downloadPath}")
-
-if SIMPLER_DOWNLOADER:
-    os.chdir(downloadPath)
-    # current path './test/'
-    if not DEBUG and os.system("git checkout simpler-downloader") != 0:
-        print("Failed checkout, impossible to use branch 'simpler-downloader'")
+if USE_GIT:
+    checkInstall("git")
+    print("Cloning repository...")
+    if not DEBUG and os.system(f"git clone {REPO_PATH}.git {downloadPath}") != 0:
+        print("Failed to clone repository, please check you internet connection and retry installation.")
         exit(-1)
-    print("Switched to branch simpler-downloader")
-    currentPath = os.path.join("./", "difd")
-    os.chdir(currentPath)
-    print("Checking config.py for dependencies...")
-    configPyPath = "config.py"
-    if not os.path.exists(configPyPath):
-        print(f"{configPyPath} not found, impossible to download dependencies.")
-        exit(-1)
-    # Dynamic import for config.py
-    spec = importlib.util.spec_from_file_location("config", configPyPath)
-    config = importlib.util.module_from_spec(spec)
-    sys.modules["config"] = config
-    spec.loader.exec_module(config)
-    dependencies: list[str] = config.DEPENDENCIES
-    print("Installing required packages...")
-    print(f"Using {PYTHON_PREFIX}")
-    installedDeps = []
-    for dependency in dependencies:
-        print(f"Installing {dependency}...")
-        if os.system(f"{PYTHON_PREFIX} -m pip install {dependency}") != 0:
-            print(f"Failed to install {dependency}, you will need to install it before running DiFD.")
-            if EXIT_ON_FAILED_DEPENDENCY_INSTALL: exit(-1)
-        installedDeps.append(dependency)
-    if len(installedDeps) != len(dependencies):
-        print("Missing dependencies:")
-        for dep in dependencies:
-            if dep not in installedDeps:
-                print(f"\t- {dep}")
-    elif len(installedDeps) == 0:
-        print(f"Nothing installed. Check you internet connection or try the installation steps manually.")
-    else:
-        print("Successfully installed\n\t" + "\n\t- ".join(installedDeps))
+    print(f"Cloned repository into {downloadPath}")
+    if SIMPLER_DOWNLOADER:
+        print("Switching to branch simpler-downloader...")
+        os.chdir(downloadPath)
+        # current path './test/'
+        if not DEBUG and os.system("git checkout simpler-downloader") != 0:
+            print("Failed checkout, impossible to use branch 'simpler-downloader'")
+            exit(-1)
+        print("Switched to branch simpler-downloader")
+        currentPath = os.path.join("./", "difd")
+        os.chdir(currentPath)
+        print("Checking config.py for dependencies...")
+        configPyPath = "config.py"
+        if not os.path.exists(configPyPath):
+            print(f"{configPyPath} not found, impossible to download dependencies.")
+            exit(-1)
+        # Dynamic import for config.py
+        spec = importlib.util.spec_from_file_location("config", configPyPath)
+        config = importlib.util.module_from_spec(spec)
+        sys.modules["config"] = config
+        spec.loader.exec_module(config)
+        dependencies: list[str] = config.DEPENDENCIES
+        print("Installing required packages...")
+        print(f"Using {PYTHON_PREFIX}")
+        installedDeps = []
+        for dependency in dependencies:
+            print(f"Installing {dependency}...")
+            if os.system(f"{PYTHON_PREFIX} -m pip install {dependency}") != 0:
+                print(f"Failed to install {dependency}, you will need to install it before running DiFD.")
+                if EXIT_ON_FAILED_DEPENDENCY_INSTALL: exit(-1)
+            installedDeps.append(dependency)
+        if len(installedDeps) != len(dependencies):
+            print("Missing dependencies:")
+            for dep in dependencies:
+                if dep not in installedDeps:
+                    print(f"\t- {dep}")
+        elif len(installedDeps) == 0:
+            print(f"Nothing installed. Check you internet connection or try the installation steps manually.")
+        else:
+            print("Successfully installed\n\t" + "\n\t- ".join(installedDeps))
+else:
+    checkInstall("curl")
+    
